7247c6bd6a674be9d9e2b7386808cf88f3bb2d1b
Hrishikesh Mahamuni <hrushikeshm12@gmail.com>
2026-07-18 13:23:02 +0530

    SOCKET.IO VALUE CHANGED Imported .env

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -1,13 +1,16 @@
 import { Server } from "socket.io";
 import  http  from "http";
 import express from 'express';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();  
 
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: process.env.VITE_API_URL,
+        origin: process.env.CLIENT_URL,
         methods: ["GET", "POST"]
     }
 });
